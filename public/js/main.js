310b240cdd4d7312696f372db50a5554ba6aea62
Kendra Cook <kendracook@Kendras-MacBook-Air.local>
2018-07-19 12:48:05 -0700

    updated copy

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,9 @@
 
 function myFunction() 
-document.getElementById("details")
 {
-<h2> Castro Fountain </h2>
-<p>Step back in time and enjoy a classic ice cream sundae at <a href="https://www.thecastrofountain.com/">The Castro Fountain</a>, in the heart of the Castro.
-  <img src="https://static.urbandaddy.com/uploads/assets/image/articles/standard/babd41633979f77450020d896e14de12.jpg"/>
-
+document.getElementById("details").innerHTML = "#1 in SF: Gio Gelati";
+<h2> Gio Gelati </h2>
+<p>This recent addition in Cow Hollow can't be beat <a href="https://www.thecastrofountain.com/">The Castro Fountain</a>, in the heart of the Castro.
+  <img src="https://hoodwork-production.s3.amazonaws.com/uploads/story/image/57856/IMG_0389.jpg"/>
 }
 
